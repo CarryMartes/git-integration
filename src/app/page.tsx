9720f43ea6faed43d6dc216d601cd6d9d4783225
Zhahangir <ZhBayanov@kazatomprom.kz>
2024-06-19 18:35:50 +0600

    tailwind fixed & input file extended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     })
       return (
           <div className={'flex items-center justify-center h-[100vh]'}>
-            <form className="max-w-sm mx-auto">
+            <div className="max-w-sm mx-auto">
               <div className="mb-5">
                 <Input value={form.token}
                        onChange={(e) => setForm({...form, token: e.target.value})}
@@ -21,14 +21,14 @@ export default function Home() {
                 <Input label={'Github Username'}
                        onChange={(e) => setForm({...form, username: e.target.value})}/>
               </div>
-              <button type="submit"
+              <button type="button"
                       onClick={() => {
                           router.push(`/repos?token=${form.token}&username=${form.username}`)
                       }}
                       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Show repositories
               </button>
-            </form>
+            </div>
           </div>
       )
 }
--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -7,16 +7,11 @@ interface IProps extends InputHTMLAttributes<any> {
 export function Input({label, ...props}: IProps) {
     return (
         <>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-            <input type="email" id="email"
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+            <input type="text"
                    className="bg-gray-50
                         border border-gray-300
-                        text-gray-900 text-sm rounded-lg
-                         focus:ring-blue-500 focus:border-blue-500
-                         block w-full p-2.5 dark:bg-gray-700
-                         dark:border-gray-600 dark:placeholder-gray-400
-                         dark:text-white dark:focus:ring-blue-500
-                         dark:focus:border-blue-500"
+                        text-gray-900 text-lg rounded-lg w-full pt-2 pb-2 pr-2 pl-2"
                    {...props}
                    />
         </>
